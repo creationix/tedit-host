41d5c04e1be4595bebad963913d9a275ce4c5c02
Tim Caswell <tim@creationix.com>
2014-03-01 18:10:21 +0000

    Get more git operations working in node.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ console.log(repo);
 repo.readRef("refs/heads/master", function (err, ref) {
   if (err) throw err;
   console.log("MASTER", ref);
+  repo.loadAs("commit", ref, function (err, commit) {
+    if (err) throw err;
+    console.log("COMMIT", commit);
+  });
 });
 
 // Create repos from githubName like `creationix/tedit-host`
@@ -18,8 +22,8 @@ function mountGithub(githubName) {
   require('js-git/mixins/github-db')(repo, githubName, githubToken);
   // Github has this built-in, but it's currently very buggy
   require('js-git/mixins/create-tree')(repo);
-  // Cache github objects locally in indexeddb
-  require('js-git/mixins/add-cache')(repo, require('js-git/mixins/indexed-db'));
+  // // Cache github objects locally in indexeddb
+  // require('js-git/mixins/add-cache')(repo, require('js-git/mixins/level-db'));
 
   // Cache everything except blobs over 100 bytes in memory.
   require('js-git/mixins/mem-cache')(repo);
